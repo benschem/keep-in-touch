77cde031e7b0cdaa236896c5f220a57d3a16bd1b
Ben Schembri <benschembri@gmail.com>
2024-07-25 14:21:54 +1000

    controllers written

--- app/controllers/interaction_controller.rb
+++ app/controllers/interaction_controller.rb
@@ -1,2 +1,50 @@
 class InteractionsController < ApplicationController
+  before_action :set_person, only: %i[create update destroy]
+  before_action :set_interaction, only: %i[update destroy]
+
+  # POST /people/1/interactions
+  def create
+    @interaction = @person.interactions.new(interaction_params)
+    if @interaction.save
+      redirect_to @person, notice: 'Interaction was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /people/1/interactions/1
+  def update
+    if @interaction.update(interaction_params)
+      redirect_to @person, notice: 'Interaction was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /people/1/interactions/1
+  def destroy
+    if @interaction.destroy
+      redirect_to @person, notice: 'Interaction was successfully destroyed.'
+    else
+      redirect_to @person, alert: 'Interaction could not be destroyed.'
+    end
+  end
+
+  private
+
+  def set_person
+    @person = Person.includes(:interactions).find(params[:person_id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to people_path, alert: 'Person not found.'
+  end
+
+  def set_interaction
+    @interaction = @person.interactions.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to @person, alert: 'Interaction not found.'
+  end
+
+  def interaction_params
+    params.require(:interaction).permit(:date, :method, :initiated_by, :context)
+  end
 end
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,53 @@
 class PeopleController < ApplicationController
+  before_action :set_person, only: %i[show update destroy]
+
+  # GET /people
+  def index
+    @people = Person.includes(:interactions).all
+  end
+
+  # GET /people/1
+  def show
+  end
+
+  # POST /people
+  def create
+    @person = Person.new(person_params)
+
+    if @person.save
+      redirect_to @person, notice: 'Person was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /people/1
+  def update
+    if @person.update(person_params)
+      redirect_to @person, notice: 'Person was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /people/1
+  def destroy
+    if @person.destroy
+      redirect_to people_url, notice: 'Person was successfully deleted.'
+    else
+      redirect_to @person, notice: 'Person could not be deleted.'
+    end
+  end
+
+  private
+
+  def set_person
+    @person = Person.includes(:interactions).find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to people_url, alert: 'Person not found.'
+  end
+
+  def person_params
+    params.require(:person).permit(:name, :relationship)
+  end
 end
