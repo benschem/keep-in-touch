1d136502e8ae65d59f73040ebccbb7c3ba0ae3e5
Ben Schembri <benschembri@gmail.com>
2024-07-26 11:34:22 +1000

    fixed typo and added email validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :people, dependent: :destroy
-  has_many :interactions, :through :people
+  before_save :downcase_email
 
+  has_many :people, dependent: :destroy
+  has_many :interactions, through: :people
+
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
